f68a7be2ce7321510b8fafd2652d835beab5e5b7
Radserhii <radserhii@gmail.com>
2018-01-16 11:20:58 +0200

    Added function for format files size from bytes

--- script.js
+++ script.js
@@ -13,7 +13,8 @@ function handleFilesSelect (e) {
 
         td1.innerHTML = file.name;
         tr.appendChild(td1);
-        td2.innerHTML = file.size;
+        td2.innerHTML = formatSizeFromBytes(file.size);
+        console.log(td2);
         tr.appendChild(td2);
 
         if(file.type.match('image.*')) {
@@ -30,3 +31,34 @@ function handleFilesSelect (e) {
 }
 
 document.querySelector('input[type="file"]').addEventListener('change', handleFilesSelect);
+
+
+function formatSizeFromBytes($bytes)
+{
+    if ($bytes >= 1073741824)
+    {
+        $bytes = ($bytes / 1073741824).toFixed(2) + ' GB';
+    }
+    else if ($bytes >= 1048576)
+    {
+        $bytes = ($bytes / 1048576).toFixed(2) + ' MB';
+    }
+    else if ($bytes >= 1024)
+    {
+        $bytes = ($bytes / 1024).toFixed(2) + ' KB';
+    }
+    else if ($bytes > 1)
+    {
+        $bytes = $bytes + ' bytes';
+    }
+    else if ($bytes == 1)
+    {
+        $bytes = $bytes + ' byte';
+    }
+    else
+    {
+        $bytes = '0 bytes';
+    }
+
+    return $bytes;
+}
